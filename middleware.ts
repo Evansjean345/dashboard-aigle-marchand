62814b7f6f86792024b6e4d4caee62a7f8541cfd
evansjean345 <evansjean345@gmail.com>
2025-01-12 14:52:52 +0000

    url

--- middleware.ts
+++ middleware.ts
@@ -4,35 +4,19 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  // Gestion des redirections basées sur les cookies
   if (
     (pathname === "/login" || pathname === "/register") &&
     request.cookies.has("userAuth")
-  ) {
+  )
     return NextResponse.redirect(new URL("/", request.url));
-  }
 
   if (
     (pathname === "/" || pathname === "/accounts") &&
     !request.cookies.has("userAuth")
-  ) {
+  )
     return NextResponse.redirect(new URL("/login", request.url));
-  }
 
-  // Ajouter les en-têtes CORS pour autoriser les appels API
-  const response = NextResponse.next();
-  response.headers.set("Access-Control-Allow-Origin", "*"); // Ou spécifiez l'origine
-  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (request.method === "OPTIONS") {
-    return new Response(null, {
-      status: 204,
-      headers: response.headers,
-    });
-  }
-
-  return response;
+  return NextResponse.next();
 }
 
 export const config = {
